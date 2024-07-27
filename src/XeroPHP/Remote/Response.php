f52be848df18cc9327d6174420cb9648854cc76f
Gordon Zhao <gordon@devcon.tech>
2024-07-27 20:30:32 +1000

    Update Response.php

--- src/XeroPHP/Remote/Response.php
+++ src/XeroPHP/Remote/Response.php
@@ -312,6 +312,10 @@ class Response
         /** @var SimpleXMLElement $root_child */
         foreach ($sxml as $child_index => $root_child) {
             switch ($child_index) {
+                case 'PageInfo':
+                case 'pagination':
+                    // TODO: We can potentially handle the page info and make it a value on the response object
+                    break;
                 case 'ErrorNumber':
                     $this->root_error['code'] = (string)$root_child;
 
@@ -349,6 +353,10 @@ class Response
 
         foreach ($json as $child_index => $root_child) {
             switch ($child_index) {
+                case 'PageInfo':
+                case 'pagination':
+                    // TODO: We can potentially handle the page info and make it a value on the response object
+                    break;
                 case 'ErrorNumber':
                     $this->root_error['code'] = $root_child;
 
